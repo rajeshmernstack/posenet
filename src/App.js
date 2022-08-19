85ed9a6237f2c3412a3ae9ce25b6fa68614f31b4
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-08-19 12:46:09 +0500

    Bug Fixed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Webcam from 'react-webcam';
 import { drawKeypoints, drawSkeleton } from './utilities';
 import '@tensorflow/tfjs-backend-webgl';
 
+
 import './App.css';
 
 function App() {
@@ -13,32 +14,31 @@ function App() {
   //Load Posenet
   const runPosenet = async () => {
     const net = await posenet.load({
-      inputResolution: {width:640, height:480},
+      inputResolution: { width: 640, height: 480 },
       scale: 0.5
     })
     // Set Interval
-    setInterval(() =>{
+    setInterval(() => {
       detect(net)
     }, 100)
   };
   const detect = async (net) => {
-    if(typeof webcamRef.current !== 'undefined' && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
+    if (typeof webcamRef.current !== 'undefined' && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
       // Get Video Properties
       const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.width;
-      const videoHeight = webcamRef.current.video.height;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
 
       // Set Video Width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
       const pose = await net.estimateSinglePose(video);
-      console.log(pose);
 
       drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
     }
-  }
 
+  }
   const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
     const ctx = canvas.current.getContext('2d');
     canvas.current.width = videoWidth;
@@ -47,6 +47,8 @@ function App() {
     drawKeypoints(pose["keypoints"], 0.5, ctx);
     drawSkeleton(pose["keypoints"], 0.5, ctx);
   }
+
+
 
   runPosenet();
   return (
